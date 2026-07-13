0267fd61569618b3326e3e43249a843255f3de59
mastan shareef <mastanshareefshaik5@gmail.com>
2026-07-13 15:31:36 +0530

    initial status

---
+++ src/components/StudentDetails.jsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom"
+function StudentDetails() {
+    const {id} = useParams();
+    return(
+        <div>
+            <h1>Student Details</h1>
+            <hr />
+            <h2>Student Id: {id}</h2>
+          
+        </div>
+    )
+}
+export default StudentDetails;
